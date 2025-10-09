5b8d6557470d5d59d6c2b242113f972051d2b17a
Santiago <santiagobtto30@gmail.com>
2025-10-09 10:53:15 -0300

    +Ordene el lector_archivos

--- componentes/lector_archivos.py
+++ componentes/lector_archivos.py
@@ -1,26 +1,25 @@
-RUTA_ARCHIVO = r"C:\Users\ezeme\OneDrive\Desktop\Facultad\Python\PracticoIntegrador-Menendez-Barretto\Paises.csv" #Python ignora los caracteres de escape
-#RUTA_ARCHIVO = r"C:\Users\Santiago\PracticoIntegrador-Menendez-Barretto\Paises.csv"
+#RUTA_ARCHIVO = r"C:\Users\ezeme\OneDrive\Desktop\Facultad\Python\PracticoIntegrador-Menendez-Barretto\Paises.csv" #Python ignora los caracteres de escape
+RUTA_ARCHIVO = r"C:\Users\Santiago\PracticoIntegrador-Menendez-Barretto\Paises.csv"
 
 def leer_archivo():
     try:
-        # Abrimos el archivo en modo lectura
-        archivo = open(RUTA_ARCHIVO, "r", encoding="utf-8")
-        
-        print("Contenido del archivo:\n")
+        with open(RUTA_ARCHIVO, "r", encoding="utf-8") as archivo:
+            paises = []
+            inicio = True
 
-        paises = []
+            for linea in archivo:
+                linea = linea.strip()
+                if not inicio:  # salta el encabezado
+                    nombre, poblacion, superficie, continente = linea.split(";")
+                    paises.append({
+                        "nombre": nombre,
+                        "poblacion": int(poblacion),
+                        "superficie": int(superficie),
+                        "continente": continente
+                    })
+                inicio = False
 
-        inicio = True
-        # Recorremos línea por línea
-        for linea in archivo:
-            linea = linea.strip()
-            if inicio == False:
-                nombre, poblacion, superficie, continente = linea.split(";")
-                paises.append({"nombre": nombre, "poblacion": poblacion, "superficie": superficie, "continente": continente})
-
-            inicio = False
-
-        return paises
+            return paises
 
     except FileNotFoundError:
         print(f"Error: no se encontró el archivo '{RUTA_ARCHIVO}'. Verificá la ruta o el nombre.")
@@ -33,3 +32,9 @@ def leer_archivo():
             archivo.close()
         except NameError:
             pass
+
+
+# Prueba
+paises = leer_archivo()
+for p in paises:
+    print(p)
