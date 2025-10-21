ff21b539aba6b944212fae3a8d89b809ec9a9781
Ezequiel Menéndez <ezemenendez2003@gmail.com>
2025-10-20 21:41:38 -0300

    +Agregando modularización extra

--- componentes/funciones.py
+++ componentes/funciones.py
@@ -1,41 +1,11 @@
-#Función para ver países
-def ver_paises(paises):
-    """"
-        Recibe una lista de paises y los recorre para listarlos
-    """
-    separador()
-    print("\nLISTA DE PAÍSES:\n")
-    for pais in paises:
-        print(f"{pais['nombre']} - {pais['poblacion']} habitantes - {pais['superficie']} km2 - {pais['continente']}")
-    tecla_para_continuar()
-
-#Función para buscar un país
-def buscar_pais(paises, nombre_buscado):
-    """"
-        Recibe una lista de paises y la busqueda del nombre del país.
-        Recorre la lista de los paises y busca si el nombre coincide.
-        Muestra las coincidencias.
-    """
-    separador()
-    print(f"\nBUSCANDO PAÍSES... '{nombre_buscado}'\n")
-    encontrados = []
-    for pais in paises:
-        if nombre_buscado.lower() in pais['nombre'].lower():
-            encontrados.append(pais)
-    if len(encontrados) == 0:
-        print("No se encontraron países.")
-        tecla_para_continuar()
-    else:
-        ver_paises(encontrados)
-
 #Función para filtar por continente
 def filtrar_por_continente(paises, continente):
-    """"
+    """
         Recibe una lista de paises y el continente.
         Recorre la lista de los paises y busca si el continente coincide.
         Muestra las coincidencias.
     """
-    print(f"\nPAÍSES DEL CONTINENTE: {continente}\n")
+    titulo(f"PAÍSES DEL CONTINENTE: {continente}")
     encontrados = []
     for pais in paises:
         if pais['continente'].lower() == continente.lower():
@@ -43,18 +13,18 @@ def filtrar_por_continente(paises, continente):
     
     if len(encontrados) == 0:
         print(f"No se encontraron países en el continente {continente}.")
-        tecla_para_continuar()
     else:
-        ver_paises(encontrados)
+        listar_paises(encontrados)
+    tecla_para_continuar()
 
 #Función para filtrar por población
 def filtrar_por_poblacion(paises, min_pob, max_pob):
-    """"
+    """
         Recibe una lista de paises, el mínimo de población, y el máximo de población
         Recorre la lista de los paises y busca los países entre el mínimo y el máximo de población
         Muestra las coincidencias.
     """
-    print(f"\nPAÍSES CON POBLACION ENTRE {min_pob} Y {max_pob}\n")
+    titulo(f"PAÍSES CON POBLACION ENTRE {min_pob} Y {max_pob}")
     encontrados = []
     for pais in paises:
         if min_pob <= pais['poblacion'] <= max_pob:
@@ -62,18 +32,18 @@ def filtrar_por_poblacion(paises, min_pob, max_pob):
     
     if len(encontrados) == 0:
         print(f"No se encontraron países con población entre {min_pob} y {max_pob}.")
-        tecla_para_continuar()
     else:
-        ver_paises(encontrados)
+        listar_paises(encontrados)
+    tecla_para_continuar()
 
 #Función para filtrar por superficie
 def filtrar_por_superficie(paises, min_sup, max_sup):
-    """"
+    """
         Recibe una lista de paises, el mínimo de superficie, y el máximo de superficie
         Recorre la lista de los paises y busca los países entre el mínimo y el máximo de superficie
         Muestra las coincidencias.
     """
-    print(f"\nPAÍSES CON UNA SUPERFICIE ENTRE {min_sup} Y {max_sup} KM2\n")
+    titulo(f"PAÍSES CON UNA SUPERFICIE ENTRE {min_sup} Y {max_sup} KM2")
     encontrados = []
     for pais in paises:
         if min_sup <= pais['superficie'] <= max_sup:
@@ -81,71 +51,29 @@ def filtrar_por_superficie(paises, min_sup, max_sup):
     
     if len(encontrados) == 0:
         print(f"No se encontraron países con una superficie entre {min_sup} y {max_sup}.")
-        tecla_para_continuar()
     else:
-        ver_paises(encontrados)
+        listar_paises(encontrados)
+    tecla_para_continuar()
 
 #Función para ordenar países
 def ordenar_paises(paises, tipo, descendente = False):
-    """"
+    """
         Recibe una lista de paises, el tipo de ordenamiento y si el orden es descendente o no
         Evalua el tipo de ordenamiento y realiza el ordenamiento según el descendente
     """
-    print(f"\nORDENANDO POR {tipo.upper()}...\n")
+    titulo(f"ORDENANDO POR {tipo.upper()}...")
 
     # Copiamos la lista para no modificar la original
     lista_ordenada = paises.copy()
     # key=lambda es una funcion anonima que toma un parametro p (país) y devuelve p['nombre'], se usa en diccionarios.
     lista_ordenada.sort(key=lambda p: p[tipo], reverse=descendente) # Ordena alfabeticamente por según el tipo
 
-    for p in lista_ordenada:
-        print(f"{p['nombre']} - {p['poblacion']} habitantes - {p['superficie']} km2 - {p['continente']}")
+    listar_paises(lista_ordenada)
     tecla_para_continuar()
 
-#Función para mostrar estadisticas
-def mostrar_estadisticas(paises):
-    """
-        Recibe la lista de paises
-        Calcula las diferentes estadicticas y muestra los resultados
-    """
-    separador()
-    print("\nESTADÍSTICAS:\n")
-
-    mayor_pob = paises[0]
-    menor_pob = paises[0]
-    total_pob = 0
-    total_sup = 0
-    contador_continentes = {}
-
+def listar_paises(paises):
     for pais in paises:
-        # Calcular mayor y menor
-        if pais['poblacion'] > mayor_pob['poblacion']:
-            mayor_pob = pais
-        if pais['poblacion'] < menor_pob['poblacion']:
-            menor_pob = pais
-
-        # Sumar para promedios
-        total_pob += pais['poblacion']
-        total_sup += pais['superficie']
-
-        # Contar por continente
-        cont = pais['continente']
-        if cont in contador_continentes:
-            contador_continentes[cont] += 1
-        else:
-            contador_continentes[cont] = 1
-
-    promedio_pob = total_pob / len(paises)
-    promedio_sup = total_sup / len(paises)
-
-    print(f"País con mayor población: {mayor_pob['nombre']} ({mayor_pob['poblacion']})")
-    print(f"País con menor población: {menor_pob['nombre']} ({menor_pob['poblacion']})")
-    print(f"Promedio de población: {promedio_pob:.0f}")
-    print(f"Promedio de superficie: {promedio_sup:.0f}")
-    print("\nCantidad de países por continente:")
-    for c, cantidad in contador_continentes.items():
-        print(f"{c}: {cantidad}")
-    tecla_para_continuar()
+        print(f"{pais['nombre']} - {pais['poblacion']} habitantes - {pais['superficie']} km2 - {pais['continente']}")
 
 #Función para continuar al menú
 def tecla_para_continuar():
@@ -169,4 +97,9 @@ def pedir_entero(mensaje):
 
 #Imprime un separador
 def separador():
-    print("===================================================")
+    print("===================================================\n")
+
+def titulo(texto):
+    separador()
+    print(f"{texto}\n")
+    separador()
--- componentes/menu.py
+++ componentes/menu.py
@@ -1,6 +1,5 @@
-from componentes.funciones import *
+from componentes.opciones_menu import *
 from componentes.lector_archivos import leer_archivo
-
 
 paises = leer_archivo()
 
@@ -26,111 +25,14 @@ def menu():
         match opcion:
             case "1": # Ver todos los paises
                 ver_paises(paises)
-
             case "2": # Buscar un pais
                 buscar_pais(paises, input("Ingrese el nombre del país a buscar: "))
-
             case "3": # Submenu de filtros
-                while True:
-                    imprimir_lista([
-                        "Filtar países por:",
-                        "1. Continente",
-                        "2. Rango de población",
-                        "3. Rango de superficie",
-                        "0. Volver al menú"
-                    ])
-
-                    opcion_filtro = input("Ingrese el filtro a aplicar: ")
-
-                    match opcion_filtro:
-                        case "1": # Filtrar por continente
-                            while True:
-                                imprimir_lista([
-                                    "CONTINENTES:",
-                                    "1. América",
-                                    "2. Europa",
-                                    "3. Asia",
-                                    "4. África",
-                                    "5. Oceanía"
-                                ])
-                                continentes = {
-                                    1: "América",
-                                    2: "Europa",
-                                    3: "Asia",
-                                    4: "África",
-                                    5: "Oceanía"
-                                }
-
-                                try:
-                                    opcion_continente = int(input("Ingrese el continente a filtrar: ")) 
-                                    if 1 <= opcion_continente <= 5: # Valir que este dentro del rango 1–5
-                                        continente = continentes[opcion_continente]
-                                        filtrar_por_continente(paises, continente)
-                                        break
-                                    else:
-                                        print("Opción invalida 🔴")
-                                except ValueError:
-                                    print("⚠️ Ingrese un número válido.")
-                            break
-
-                        case "2": # Filtrar por poblacion
-                            min_pob = pedir_entero("Ingrese la población mínima: ")
-                            max_pob = pedir_entero("Ingrese la población máxima: ")
-                            filtrar_por_poblacion(paises, min_pob, max_pob)
-                            break
-
-                        case "3": # Filtrar por superficie
-                            min_sup = pedir_entero("Ingrese la superficie mínima: ")
-                            max_sup = pedir_entero("Ingrese la superficie máxima: ")
-                            filtrar_por_superficie(paises, min_sup, max_sup)
-                            break
-
-                        case "0": # Volver al menu principal
-                            print("Volviendo al menú...")
-                            break
-
-                        case _:
-                            print("Acción invalida 🔴")
-
+                menu_filtrar(paises)
             case "4": # Submenu de ordenamientos
-                while True:
-                    imprimir_lista([
-                        "Ordenar países por:",
-                        "1. Nombre",
-                        "2. Población",
-                        "3. Superficie",
-                        "0. Volver al menú"
-                    ])
-
-                    opcion_ordenar = input("Ingrese el ordenamiento a aplicar: ")
-
-                    match opcion_ordenar:
-                        case "1": # Ordenar por nombre
-                            ordenar_paises(paises, "nombre")
-                            break
-
-                        case "2": # Ordenar por poblacion
-                            descendente = input("Ingrese 1 si quiere un orden descendente o cualquier otra tecla si quiere orden ascendente: ")
-                            descendente = descendente == "1"
-                            ordenar_paises(paises, "poblacion", descendente)
-                            break
-
-                        case "3": # Ordenar por superficie
-                            descendente = input("Ingrese 1 si quiere un orden descendente o cualquier otra tecla si quiere orden ascendente: ")
-                            descendente = descendente == "1"
-                            ordenar_paises(paises, "superficie", descendente)
-                            break
-
-                        case "0": # Volver al menu principal
-                            print("Volviendo al menú...")
-                            break
-
-                        case _: # Ejecuta si no se cumplio ninguno de los anteriores 
-                            print("Acción invalida 🔴")
-
+                menu_ordenar(paises)
             case "5": # Mostrar estadisticas
                 mostrar_estadisticas(paises)
-
             case "0": # Salir del programa
                 print("Saliendo... 🌐")
                 break
